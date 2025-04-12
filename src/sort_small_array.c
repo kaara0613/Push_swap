cd57c470e5853502efdc0b388ed2adee45b76c26
kaara <arkan0613@icloud.com>
2025-04-12 18:01:08 +0900

    fix rm tester

--- src/sort_small_array.c
+++ src/sort_small_array.c
@@ -2,11 +2,6 @@
 
 bool	sort_small_case_a(long flag, struct s_stack *stack_a, struct s_stack *stack_b)
 {
-    // if (count_pivot_u(flag, stack_a) == 3)
-	// {
-    //     small_sort_u3a(flag, stack_a, stack_a);
-	// 	return (true);
-	// }
 	if (count_pivot_u(flag, stack_a) <= 2)
 	{
         small_sort_u2a(flag, stack_a, stack_b);
@@ -17,11 +12,6 @@ bool	sort_small_case_a(long flag, struct s_stack *stack_a, struct s_stack *stack
 
 bool	sort_small_case_b(long flag, struct s_stack *stack_a, struct s_stack *stack_b)
 {
-    // if (count_pivot_u(flag, stack_b) == 3)
-	// {
-    //     small_sort_u3b(flag, stack_a, stack_b);
-	// 	return (true);
-	// }
 	if (count_pivot_u(flag, stack_b) <= 2)
 	{
         small_sort_u2b(flag, stack_a, stack_b);
